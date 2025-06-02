686357af3af12171aa147e05705d151ea45b223f
James Wong <pgosnp01@gmail.com>
2025-06-02 18:47:54 -0400

    Update Projects component to include project links for enhanced navigation

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "../sub/ProjectCard";
 const Projects = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center py-10"
+      className="flex flex-col items-center justify-center py-10 relative z-10"
       id="projects"
     >
       <h1 className="text-[32px] md:text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10 md:py-20">
@@ -15,31 +15,37 @@ const Projects = () => {
           src="/TradingHub.png"
           title="Trading Hub"
           description="A Crypto Asset Management and Trading Platform. Tech Stack: VueJS, ExpressJS, MongoDB"
+          link="https://www.linkedin.com/in/shingson/overlay/1712941964511/single-media-viewer/?profileId=ACoAAChT3SIBo1Rizj9i5POZbI_jIBa9C6CQd8w"
         />
         <ProjectCard
           src="/Zirkol.png"
           title="Zirkol"
           description="A E-Commerce Cross-Platform Mobile Application. Tech Stack: Flutter, NestJS, AWS EC2, Mysql, Elastic Search"
+          link="https://www.linkedin.com/in/shingson/overlay/experience/2213501470/multiple-media-viewer/?profileId=ACoAAChT3SIBo1Rizj9i5POZbI_jIBa9C6CQd8w&treasuryMediaId=1712942166815"
         />
         <ProjectCard
           src="/JarvixPay.png"
           title="Jarvix Pay"
           description="Intelligent one-stop Payment Application. Tech Stack: VueJS, Ionic, Typescript, ExpressJS, Mysql, AWS EC2, AWS ELB, AWS S3, AWS RDS"
+          link="https://www.linkedin.com/in/shingson/overlay/experience/2213501470/multiple-media-viewer/?profileId=ACoAAChT3SIBo1Rizj9i5POZbI_jIBa9C6CQd8w&treasuryMediaId=1712942166816"
         />
         <ProjectCard
           src="/Decarbon.png"
           title="Decarbon"
           description="A Carbon-base Coupon Platform for IOS & Android. Tech Stack: Flutter, ExpressJS, Mysql, AWS Lambda, AWS API Gateway, AWS Cognito"
+          link="https://github.com/zinxon/Decarbon-Flutter"
         />
         <ProjectCard
           src="/Fanchat.png"
           title="Fanchat"
           description="Hong Kong Stock Price Prediction AI Chatbot. Tech Stack: Flutter, Python, Keras, Flask, Firebase, Dialogflow"
+          link="https://github.com/zinxon/Fanchat"
         />
         <ProjectCard
           src="/SpaceWebsite.png"
           title="NextJS Portfolio Website"
           description="James's Personal Portfolio Website. Tech Stack: NextJS, Github Actions, Vercel"
+          link="https://next-portfolio-zinxon.vercel.app/"
         />
       </div>
     </div>
